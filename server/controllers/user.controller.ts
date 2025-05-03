6bc95a5da8047bdc04754e83d0c98ff55eb71fc1
Shafat Mufdi <136893443+meowdyyy@users.noreply.github.com>
2025-05-03 23:11:47 +0600

    Sprint 3 Commit

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -179,6 +179,9 @@ export const logoutuser = CatchAsyncError(async(req:Request,res:Response, next:N
         try {
             res.cookie("access_token", "", {maxAge: 1 });
             res.cookie("refresh_token", "", {maxAge: 1 });
+            const userId = req.user?._id || "";
+            redis.del(userId);
+
             res.status(200).json({
                 success:true,
                 message: "Logged out successfully",
