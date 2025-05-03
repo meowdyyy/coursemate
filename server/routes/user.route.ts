07c14f0be3f78e3205340c0ed2503afb41b15fea
Shafat Mufdi <136893443+meowdyyy@users.noreply.github.com>
2025-05-03 23:05:51 +0600

    Sprint 3 Commit

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -5,7 +5,7 @@ const userRouter = express.Router();
 userRouter.post("/registration", registrationUser);
 userRouter.post("/activate-user", activateUser);
 userRouter.post('/login', loginUser);
-userRouter.get('/logout', logoutUser);
+userRouter.get("/logout",isAutheticated, logoutUser);
 userRouter.get("/me", isAutheticated, getUserInfo);
 userRouter.post("/social-auth", socialAuth);
 userRouter.put("/update-user-info",isAutheticated, updateUserInfo);
