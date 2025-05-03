01b8f6f2faf57aca0d240feb5b7a76fceec7e9a7
Shafat Mufdi <136893443+meowdyyy@users.noreply.github.com>
2025-05-04 00:05:23 +0600

    Sprint 3 Commit

--- server/app.ts
+++ server/app.ts
@@ -5,6 +5,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import { ErrorMiddleware } from "./middleware/error";
 import userRouter from "./routes/user.route";
+import courseRouter from "./routes/course.route";
 
 // body parser
 app.use(express.json({ limit: "50mb" }));
