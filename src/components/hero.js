81de37af2f5448e33b712d35da47ceb5443625b0
Shafat Mufdi <136893443+meowdyyy@users.noreply.github.com>
2025-05-13 18:45:45 +0600

    Created hero.js for landing page hero banner

---
+++ src/components/hero.js
@@ -0,0 +1,16 @@
+// src/components/Hero.js
+import React from 'react';
+
+const hero = () => {
+  return (
+    <div className="hero">
+      <div className="overlay-text">
+        <h1>CourseMate</h1>
+        <h3>Your One-Stop Study Resource Platform</h3>
+        <h5>Access past quizzes, exams, notes, and videos all in one place</h5>
+      </div>
+    </div>
+  );
+};
+
+export default hero;
